e08150649a43a52cb04f76aa4422447c04fa2ca7
unbyte <i@shangyes.net>
2021-08-29 14:18:09 +0800

    fix examples and the position of macros.d.ts

--- examples/provider/macros.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@echo' {
-  export interface DummyType {}
-  export function echo(msg: string, repeat?: number): void
-}
-declare module '@load' {
-  export function load(glob: string): void
-}
