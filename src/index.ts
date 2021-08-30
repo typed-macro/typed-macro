f47e688cf4f0c097ac9ede8899cdadb79d8b49bb
unbyte <i@shangyes.net>
2021-08-31 00:50:19 +0800

    :bug: export type Macro to fix TS4023

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export * from './createMacroManager'
 export { vitePluginMacro } from './plugin'
 
 // export types
-export type { MacroHandler, MacroContext } from './core/macro'
+export type { MacroHandler, MacroContext, Macro } from './core/macro'
 export type { MacroHelper } from './core/macro/helper'
 export type { Exportable, NamespacedExportable } from './core/exports'
 export type { ImportOption } from './core/helper/import'
