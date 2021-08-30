77c34d62dbf71c802e59ea2fa58b46720aa8eb82
unbyte <i@shangyes.net>
2021-08-31 01:02:53 +0800

    :package: exports more types to help build dts

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,13 @@ export * from './createMacroManager'
 export { vitePluginMacro } from './plugin'
 
 // export types
-export type { MacroHandler, MacroContext, Macro } from './core/macro'
+export type { Babel } from './core/macro/babel'
 export type { MacroHelper } from './core/macro/helper'
+export type { MacroHandler, MacroContext, Macro } from './core/macro'
+
+export type { MacroProvider, MacroProviderHooks } from './macroProvider'
+export type { MacroPlugin, MacroPluginHooks } from './macroPlugin'
+export type { MacroManager } from './macroManager'
 export type { Exportable, NamespacedExportable } from './core/exports'
 export type { ImportOption } from './core/helper/import'
 export type { DevServerHelper } from './helper/server'
