fdef113d03e6fd478c0889cb76fb1be63d004d7d
unbyte <i@shangyes.net>
2021-09-06 18:36:22 +0800

    refine comments

--- src/core/transformer.ts
+++ src/core/transformer.ts
@@ -26,8 +26,14 @@ export type TransformerOptions = {
   /**
    * Babel plugins to be applied during parsing.
    *
-   * 'typescript' and 'importMeta' for all files, 'jsx' for jsx/tsx files
+   * 'typescript', 'jsx' and 'importMeta'
    * are included by default and cannot be removed.
+   *
+   * Some plugins are enabled automatically by the latest babel parser
+   * because they have been regarded as part of the language.
+   *
+   * @see https://babeljs.io/docs/en/babel-parser#plugins
+   * @see https://babeljs.io/docs/en/babel-parser#latest-ecmascript-features
    */
   parserPlugins?: ParserPlugin[]
 }
